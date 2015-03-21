1a04b5ad8137fbf47d7f26ddceba2d133696e0a0
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2015-03-21 17:14:26 +0100

    adds hello world middleware

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express')
   , app = express()
 
+var middlewares = {
+    helloWorld : function(req, res, next) {
+        res.send('Hello World Middleware!')
+    }
+}
+
+app.get('/', middlewares.helloWorld)
+
 app.get('/', function(req, res) {
     res.send('Hello World!');
 })
